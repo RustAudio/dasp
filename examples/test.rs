2b91bbe7c86c03c4bcc326162f67f802a37ce49f
mitchmindtree <mitchell.nordine@gmail.com>
2015-02-13 17:58:15 +1100

    Added test that converts to and from a wave for each type that implements sample

---
+++ examples/test.rs
@@ -0,0 +1,57 @@
+#![feature(core)]
+
+extern crate sample;
+
+use sample::Sample;
+use std::num::Float;
+
+fn main() {
+
+    for i in (0..10).map(|i| i as f64 / 9.0) {
+        let wave = sine_wave(i);
+
+        println!("Wave {}", wave);
+
+        let sample_f32: f32 = Sample::from_wave(wave);
+        let sample_u8 : u8  = Sample::from_wave(wave);
+        let sample_u16: u16 = Sample::from_wave(wave);
+        let sample_u32: u32 = Sample::from_wave(wave);
+        let sample_i8 : i8  = Sample::from_wave(wave);
+        let sample_i16: i16 = Sample::from_wave(wave);
+        let sample_i32: i32 = Sample::from_wave(wave);
+
+        println!("\tFrom wave to sample -> f32: {:<10} u8: {:<10} u16: {:<10} u32: {:<10} i8: {:<10} i16: {:<10} i32: {:<10}",
+                 sample_f32, sample_u8, sample_u16, sample_u32, sample_i8, sample_i16, sample_i32);
+
+
+        let wave_f32 = sample_f32.to_wave();
+        let wave_u8  = sample_u8.to_wave();
+        let wave_u16 = sample_u16.to_wave();
+        let wave_u32 = sample_u32.to_wave();
+        let wave_i8  = sample_i8.to_wave();
+        let wave_i16 = sample_i16.to_wave();
+        let wave_i32 = sample_i32.to_wave();
+
+        println!("\tFrom sample to wave -> f32: {:<10} u8: {:<10} u16: {:<10} u32: {:<10} i8: {:<10} i16: {:<10} i32: {:<10}\n",
+                 wave_f32, wave_u8, wave_u16, wave_u32, wave_i8, wave_i16, wave_i32);
+
+        // Check that the conversion back to wave is the same as the original wave (with some
+        // headroom for resolution/rounding error).
+        let headroom = 0.05;
+        assert!((wave_f32 - wave).abs() < headroom, "{}", wave_f32);
+        assert!((wave_u8  - wave).abs() < headroom, "{}", wave_u8);
+        assert!((wave_u16 - wave).abs() < headroom, "{}", wave_u16);
+        assert!((wave_u32 - wave).abs() < headroom, "{}", wave_u32);
+        assert!((wave_i8  - wave).abs() < headroom, "{}", wave_i8);
+        assert!((wave_i16 - wave).abs() < headroom, "{}", wave_i16);
+        assert!((wave_i32 - wave).abs() < headroom, "{}", wave_i32);
+
+    }
+
+}
+
+/// Return a sine wave for the given phase.
+fn sine_wave<S: Sample>(phase: f64) -> S {
+    use std::f64::consts::PI_2;
+    Sample::from_wave((phase * PI_2).sin() as f32)
+}
