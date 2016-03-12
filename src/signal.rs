bcafdd229ef969a7ba35d2aa63d0a605a1784fb5
mitchmindtree <mitchell.nordine@gmail.com>
2016-03-12 18:07:14 +1100

    Remove Signal zip methods in favour of following convention of Sample and Frame with add_amp, mul_amp, offset_amp, scale_amp, offset_amp_per_channel and scale_amp_per_channel. Improve docs.

--- src/signal.rs
+++ src/signal.rs
@@ -1,4 +1,4 @@
-use {FloatSample, Frame, Sample, SelectFloat};
+use {Frame, Sample};
 use std;
 
 
@@ -16,42 +16,8 @@ pub trait Signal: Iterator + Sized
     where Self::Item: Frame,
 {
 
-    /// Produces an `Iterator` that takes two `Signal`s, steps them forward in lockstep, sums each
-    /// pair of `Frame`s together and yields the resulting `Frame`s.
-    ///
-    /// The `Iterator` will return `None` when either of the `Signal`s first yields `None`.
-    #[inline]
-    fn zip_add<S>(self, other: S) -> ZipAdd<Self, S>
-        where S: Signal<Item=Self::Item>,
-    {
-        ZipAdd {
-            signal_a: self,
-            signal_b: other,
-        }
-    }
-
-    /// Produces an `Iterator` that modulates the amplitude of `self` with `other` where `other` is
-    /// some `Signal` yielding `Frame`s with `Sample`s that implement `Amplitude` (f32 or f64).
-    ///
-    /// The `Iterator` steps both signals forward in lockstep, multiplies the amplitude of each
-    /// pair of `Frame`s together and yields the resulting `Frame`s.
-    ///
-    /// The `Iterator` will return `None` when either of the `Signal`s first yields `None`.
-    #[inline]
-    fn zip_mod_amp<S>(self, other: S) -> ZipModAmp<Self, S>
-        where S: Signal,
-              S::Item: Frame<NumChannels=<Self::Item as Frame>::NumChannels>,
-              <Self::Item as Frame>::Sample: SelectFloat<<S::Item as Frame>::Sample>,
-              <S::Item as Frame>::Sample: FloatSample,
-    {
-        ZipModAmp {
-            signal_a: self,
-            signal_b: other,
-        }
-    }
-
-    /// Produces an `Iterator` that scales the amplitude of every `Frame` in `self` by the given
-    /// `Amplitude` scalar.
+    /// Provides an iterator that yields the sum of the frames yielded by both `other` and `self`
+    /// in lock-step.
     ///
     /// # Example
     ///
@@ -61,20 +27,54 @@ pub trait Signal: Iterator + Sized
     /// use sample::Signal;
     ///
     /// fn main() {
-    ///     let frames = [[0.2], [-0.5], [-0.4], [0.3]];
-    ///     let signal = frames.iter().cloned();
-    ///     let scaled: Vec<[f32; 1]> = signal.scale_amp(2.0).collect();
-    ///     assert_eq!(scaled, vec![[0.4], [-1.0], [-0.8], [0.6]]);
+    ///     let a = [[0.2], [-0.6], [0.5]];
+    ///     let b = [[0.2], [0.1], [-0.8]];
+    ///     let a_signal = a.iter().cloned();
+    ///     let b_signal = b.iter().cloned();
+    ///     let added: Vec<[f32; 1]> = a_signal.add_amp(b_signal).collect();
+    ///     assert_eq!(added, vec![[0.4], [-0.5], [-0.3]]);
     /// }
     /// ```
     #[inline]
-    fn scale_amp<A>(self, amp: A) -> ScaleAmp<Self, A>
-        where A: FloatSample,
-              <Self::Item as Frame>::Sample: SelectFloat<A>,
+    fn add_amp<S>(self, other: S) -> AddAmp<Self, S>
+        where S: Signal,
+              S::Item: Frame<Sample=<<Self::Item as Frame>::Sample as Sample>::Signed,
+                             NumChannels=<Self::Item as Frame>::NumChannels>,
     {
-        ScaleAmp {
-            signal: self,
-            amp: amp,
+        AddAmp {
+            a: self,
+            b: other,
+        }
+    }
+
+    /// Provides an iterator that yields the product of the frames yielded by both `other` and
+    /// `self` in lock-step.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// extern crate sample;
+    ///
+    /// use sample::Signal;
+    ///
+    /// fn main() {
+    ///     let a = [[0.25], [-0.8], [-0.5]];
+    ///     let b = [[0.2], [0.5], [0.8]];
+    ///     let a_signal = a.iter().cloned();
+    ///     let b_signal = b.iter().cloned();
+    ///     let added: Vec<[f32; 1]> = a_signal.mul_amp(b_signal).collect();
+    ///     assert_eq!(added, vec![[0.05], [-0.4], [-0.4]]);
+    /// }
+    /// ```
+    #[inline]
+    fn mul_amp<S>(self, other: S) -> MulAmp<Self, S>
+        where S: Signal,
+              S::Item: Frame<Sample=<<Self::Item as Frame>::Sample as Sample>::Float,
+                             NumChannels=<Self::Item as Frame>::NumChannels>,
+    {
+        MulAmp {
+            a: self,
+            b: other,
         }
     }
 
@@ -96,10 +96,93 @@ pub trait Signal: Iterator + Sized
     /// }
     /// ```
     #[inline]
-    fn offset_amp(self, offset: <Self::Item as Frame>::Sample) -> OffsetAmp<Self> {
+    fn offset_amp(self, offset: <<Self::Item as Frame>::Sample as Sample>::Signed)
+        -> OffsetAmp<Self>
+    {
         OffsetAmp {
             signal: self,
             offset: offset,
+        }
+    }
+
+    /// Produces an `Iterator` that scales the amplitude of the sample of each channel in every
+    /// `Frame` yielded by `self` by the given amplitude.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// extern crate sample;
+    ///
+    /// use sample::Signal;
+    ///
+    /// fn main() {
+    ///     let frames = [[0.2], [-0.5], [-0.4], [0.3]];
+    ///     let signal = frames.iter().cloned();
+    ///     let scaled: Vec<[f32; 1]> = signal.scale_amp(2.0).collect();
+    ///     assert_eq!(scaled, vec![[0.4], [-1.0], [-0.8], [0.6]]);
+    /// }
+    /// ```
+    #[inline]
+    fn scale_amp(self, amp: <<Self::Item as Frame>::Sample as Sample>::Float) -> ScaleAmp<Self> {
+        ScaleAmp {
+            signal: self,
+            amp: amp,
+        }
+    }
+
+    /// Produces an `Iterator` that offsets the amplitude of every `Frame` in `self` by the
+    /// respective amplitudes in each channel of the given `amp_frame`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// extern crate sample;
+    ///
+    /// use sample::Signal;
+    ///
+    /// fn main() {
+    ///     let frames = [[0.5, 0.3], [-0.25, 0.9]];
+    ///     let mut signal = frames.iter().cloned().offset_amp_per_channel([0.25, -0.5]);
+    ///     assert_eq!(signal.next().unwrap(), [0.75, -0.2]);
+    ///     assert_eq!(signal.next().unwrap(), [0.0, 0.4]);
+    /// }
+    /// ```
+    #[inline]
+    fn offset_amp_per_channel<F>(self, amp_frame: F) -> OffsetAmpPerChannel<Self, F>
+        where F: Frame<Sample=<<Self::Item as Frame>::Sample as Sample>::Signed,
+                       NumChannels=<Self::Item as Frame>::NumChannels>,
+    {
+        OffsetAmpPerChannel {
+            signal: self,
+            amp_frame: amp_frame,
+        }
+    }
+
+    /// Produces an `Iterator` that scales the amplitude of every `Frame` in `self` by the
+    /// respective amplitudes in each channel of the given `amp_frame`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// extern crate sample;
+    ///
+    /// use sample::Signal;
+    ///
+    /// fn main() {
+    ///     let frames = [[0.2, -0.5], [-0.4, 0.3]];
+    ///     let mut signal = frames.iter().cloned().scale_amp_per_channel([0.5, 2.0]);
+    ///     assert_eq!(signal.next().unwrap(), [0.1, -1.0]);
+    ///     assert_eq!(signal.next().unwrap(), [-0.2, 0.6]);
+    /// }
+    /// ```
+    #[inline]
+    fn scale_amp_per_channel<F>(self, amp_frame: F) -> ScaleAmpPerChannel<Self, F>
+        where F: Frame<Sample=<<Self::Item as Frame>::Sample as Sample>::Float,
+                       NumChannels=<Self::Item as Frame>::NumChannels>,
+    {
+        ScaleAmpPerChannel {
+            signal: self,
+            amp_frame: amp_frame,
         }
     }
 
@@ -110,6 +193,59 @@ pub trait Signal: Iterator + Sized
 #[derive(Clone)]
 pub struct Equilibrium<F> {
     frame_type: std::marker::PhantomData<F>,
+}
+
+/// An iterator that yields the sum of the frames yielded by both `other` and `self` in lock-step.
+#[derive(Clone)]
+pub struct AddAmp<A, B> {
+    a: A,
+    b: B,
+}
+
+/// An iterator that yields the product of the frames yielded by both `other` and `self` in
+/// lock-step.
+#[derive(Clone)]
+pub struct MulAmp<A, B> {
+    a: A,
+    b: B,
+}
+
+/// Provides an iterator that offsets the amplitude of every channel in each frame of the
+/// signal by some sample value and yields the resulting frames.
+#[derive(Clone)]
+pub struct OffsetAmp<S>
+    where S: Signal,
+          S::Item: Frame,
+{
+    signal: S,
+    offset: <<S::Item as Frame>::Sample as Sample>::Signed,
+}
+
+/// An `Iterator` that scales the amplitude of the sample of each channel in every `Frame` yielded
+/// by `self` by the given amplitude.
+#[derive(Clone)]
+pub struct ScaleAmp<S>
+    where S: Signal,
+          S::Item: Frame,
+{
+    signal: S,
+    amp: <<S::Item as Frame>::Sample as Sample>::Float,
+}
+
+/// An `Iterator` that scales the amplitude of every `Frame` in `self` by the respective amplitudes
+/// in each channel of the given `amp` `Frame`.
+#[derive(Clone)]
+pub struct OffsetAmpPerChannel<S, F> {
+    signal: S,
+    amp_frame: F,
+}
+
+/// An `Iterator` that scales the amplitude of every `Frame` in `self` by the respective amplitudes
+/// in each channel of the given `amp` `Frame`.
+#[derive(Clone)]
+pub struct ScaleAmpPerChannel<S, F> {
+    signal: S,
+    amp_frame: F,
 }
 
 /// An iterator that takes two `Signal`s, steps them forward in lockstep, sums each pair of
@@ -130,25 +266,6 @@ pub struct ZipAdd<A, B> {
 pub struct ZipModAmp<A, B> {
     signal_a: A,
     signal_b: B,
-}
-
-/// Produces an `Iterator` that scales the amplitude of every `Frame` in `self` by the given
-/// `Amplitude` scalar.
-#[derive(Clone)]
-pub struct ScaleAmp<S, A> {
-    signal: S,
-    amp: A,
-}
-
-/// Provides an iterator that offsets the amplitude of every channel in each frame of the
-/// signal by some sample value and yields the resulting frames.
-#[derive(Clone)]
-pub struct OffsetAmp<S>
-    where S: Signal,
-          S::Item: Frame,
-{
-    signal: S,
-    offset: <S::Item as Frame>::Sample,
 }
 
 
@@ -184,45 +301,55 @@ impl<F> Iterator for Equilibrium<F>
     }
 }
 
-impl<A, B> Iterator for ZipAdd<A, B>
-    where A: Signal,
-          B: Signal<Item=A::Item>,
-          A::Item: Frame,
-{
-    type Item = A::Item;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.signal_a.next().and_then(|f_a| self.signal_b.next().map(|f_b| f_a.add_frame(f_b)))
-    }
-}
-
-impl<A, B> Iterator for ZipModAmp<A, B>
+impl<A, B> Iterator for AddAmp<A, B>
     where A: Signal,
           B: Signal,
           A::Item: Frame,
-          B::Item: Frame<NumChannels=<A::Item as Frame>::NumChannels>,
-          <A::Item as Frame>::Sample: SelectFloat<<B::Item as Frame>::Sample>,
-          <B::Item as Frame>::Sample: FloatSample,
+          B::Item: Frame<Sample=<<A::Item as Frame>::Sample as Sample>::Signed,
+                         NumChannels=<A::Item as Frame>::NumChannels>,
 {
     type Item = A::Item;
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.signal_a.next().and_then(|f_a| self.signal_b.next().map(|f_b| {
-            f_a.zip_map(f_b, |s_a, s_b| s_a.scale_amp(s_b))
-        }))
+        self.a.next().and_then(|a_f| self.b.next().map(|b_f| a_f.add_amp(b_f)))
     }
 }
 
-impl<S, A> Iterator for ScaleAmp<S, A>
+impl<A, B> Iterator for MulAmp<A, B>
+    where A: Signal,
+          B: Signal,
+          A::Item: Frame,
+          B::Item: Frame<Sample=<<A::Item as Frame>::Sample as Sample>::Float,
+                         NumChannels=<A::Item as Frame>::NumChannels>,
+{
+    type Item = A::Item;
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.a.next().and_then(|a_f| self.b.next().map(|b_f| a_f.mul_amp(b_f)))
+    }
+}
+
+impl<S> Iterator for ScaleAmp<S>
     where S: Signal,
           S::Item: Frame,
-          A: FloatSample,
-          <S::Item as Frame>::Sample: SelectFloat<A>,
 {
     type Item = S::Item;
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         self.signal.next().map(|f| f.scale_amp(self.amp))
+    }
+}
+
+impl<S, F> Iterator for ScaleAmpPerChannel<S, F>
+    where S: Signal,
+          S::Item: Frame,
+          F: Frame<Sample=<<S::Item as Frame>::Sample as Sample>::Float,
+                   NumChannels=<S::Item as Frame>::NumChannels>,
+{
+    type Item = S::Item;
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.signal.next().map(|f| f.mul_amp(self.amp_frame))
     }
 }
 
@@ -234,5 +361,18 @@ impl<S> Iterator for OffsetAmp<S>
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         self.signal.next().map(|f| f.offset_amp(self.offset))
+    }
+}
+
+impl<S, F> Iterator for OffsetAmpPerChannel<S, F>
+    where S: Signal,
+          S::Item: Frame,
+          F: Frame<Sample=<<S::Item as Frame>::Sample as Sample>::Signed,
+                   NumChannels=<S::Item as Frame>::NumChannels>,
+{
+    type Item = S::Item;
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.signal.next().map(|f| f.add_amp(self.amp_frame))
     }
 }
