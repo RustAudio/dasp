3534147c7652046168a4899c651a6ae8c979dbfa
mitchmindtree <mitchell.nordine@gmail.com>
2016-06-08 14:21:26 +1000

    On `Type` trait, move Sample type param from trait signature to `at_phase` method signature.
    Remove use of `FromSample` bounds in favour of using `Sample`'s
    associated `Float` type where necessary.
    
    Change `WindowedFrame` to `Windowed` to remove possible ambiguity with
    `Frame` trait.
    
    Make `Windowed` type generic over any `Signal` type rather than slices.
    
    Add `Windower::rectangle` and `Windower::hanning` constructors.
    
    Add documentation for public types, traits, methods, functions.

--- src/window.rs
+++ src/window.rs
@@ -1,73 +1,93 @@
+use {Float, Sample};
+use core;
 use core::marker::PhantomData;
-use core::f64;
-
-use {Sample, FloatSample};
-use signal;
-use signal::{Phase, ConstHz};
-use conv::{FromSample, ToSample};
 use frame::Frame;
+use signal::{self, Signal};
 
-// Using types instead of enum allows for static dispatch
-pub trait Type<S: Sample> {
-    fn at_phase(phase: S) -> S;
+
+/// The window function used within a `Window`.
+pub trait Type {
+    /// Returns the amplitude for the given phase, given as some `Sample` type.
+    fn at_phase<S: Sample>(phase: S) -> S;
 }
 
+
+/// A type of window function, also known as teh "raised cosine window".
+///
+/// [Wiki entry](https://en.wikipedia.org/wiki/Window_function#Hann_.28Hanning.29_window).
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Hanning;
 
+/// The simplest window type, equivalent to replacing all but *N* values of data sequence by
+/// zeroes, making it appear as though the waveform suddenly turns on and off.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Rectangle;
 
-pub struct Window<F, WT> 
+/// A `Signal` type that for every yielded `phase`, yields the amplitude across the `window::Type`
+/// for that phase.
+#[derive(Clone)]
+pub struct Window<F, W> 
     where F: Frame,
-          WT: Type<F::Sample>
+          W: Type,
 {
-    pub phase: Phase<ConstHz>,
+    /// Yields phase stepped at a constant rate to be passed to the window function `W`.
+    pub phase: signal::Phase<signal::ConstHz>,
     ftype: PhantomData<F>,
-    wttype: PhantomData<WT>
+    wttype: PhantomData<W>
 }
 
-pub struct WindowedFrame<'a, F, WT> 
-    where F: 'a + Frame,
-          F::Sample: FromSample<f64>, 
-          WT: Type<F::Sample>
+/// An iterator that multiplies frames yielded by some `Signal` with a `Window`.
+///
+/// Yields `Windowed` frames.
+#[derive(Clone)]
+pub struct Windowed<S, W> 
+    where S: Signal,
+          S::Item: Frame,
+          W: Type,
 {
-    pub data: &'a [F],
-    pub window: Window<F, WT>,
-    idx: usize
+    signal_with_window: core::iter::Zip<S, Window<S::Item, W>>,
 }
 
-/// Windower takes a long slice of data and generates an iterator over its frames
-pub struct Windower<'a, F, WT> 
+/// Takes a long slice of frames and yields `Windowed` chunks of size `bin` once every `hop` frames.
+#[derive(Clone)]
+pub struct Windower<'a, F, W> 
     where F: 'a + Frame, 
-          F::Sample: FromSample<f64>, 
-          WT: Type<F::Sample>
+          W: Type,
 {
+    /// The size of each `Windowed` chunk to be yielded.
     pub bin: usize,
+    /// The step size over `frames` for the start of each new `Windowed` chunk.
     pub hop: usize,
-    pub idx: usize,
-    pub data: &'a [F],
-    wttype: PhantomData<WT>
+    idx: usize,
+    frames: &'a [F],
+    wttype: PhantomData<W>
 }
 
-impl<S: Sample + ToSample<f64> + FromSample<f64>> Type<S> for Hanning {
-    fn at_phase(phase: S) -> S {
-        let v = phase.to_sample::<f64>() * f64::consts::PI * 2.;
-        (0.5f64 * (1f64 - super::cos(v))).to_sample::<S>()
+
+impl Type for Hanning {
+    fn at_phase<S: Sample>(phase: S) -> S {
+        let pi_2 = <S::Float as Float>::pi() * 2.0.to_sample();
+        let v = phase.to_float_sample() * pi_2;
+        let half: S::Float = 0.5.to_sample();
+        let one: S::Float = 1.0.to_sample();
+        (half * (one - v.cos())).to_sample::<S>()
     }
 }
 
-impl<S: Sample + FromSample<f64>> Type<S> for Rectangle {
-    #[allow(unused)]
-    fn at_phase(phase: S) -> S {
-        (1.).to_sample::<S>()
+impl Type for Rectangle {
+    fn at_phase<S: Sample>(_phase: S) -> S {
+        <S::Float as Float>::identity().to_sample::<S>()
     }
 }
 
-impl<F, WT> Window<F, WT> 
+
+impl<F, W> Window<F, W> 
     where F: Frame,
-          WT: Type<F::Sample>
+          W: Type
 {
+    /// Construct a new `Window` with the given length as a number of frames.
     pub fn new(len: usize) -> Self {
-        let step = signal::rate(len as f64 - 1.).const_hz(1.);
+        let step = signal::rate(len as f64 - 1.0).const_hz(1.0);
         Window {
             phase: signal::phase(step),
             ftype: PhantomData,
@@ -76,102 +96,119 @@ impl<F, WT> Window<F, WT>
     }
 }
 
-impl<F, WT> Iterator for Window<F, WT> 
-    where F: Frame, 
-          F::Sample: FromSample<f64>,
-          WT: Type<F::Sample>
+impl<S, W> Windowed<S, W> 
+    where S: Signal, 
+          S::Item: Frame,
+          W: Type
 {
-    type Item = F;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let v = WT::at_phase((self.phase.next_phase()).to_sample::<F::Sample>());
-        Some(F::from_fn(|_| v))
-    }
-}
-
-impl<'a, F, WT> WindowedFrame<'a, F, WT> 
-    where F: 'a + Frame, 
-          F::Sample: FromSample<f64>, 
-          WT: Type<F::Sample>
-{
-    pub fn new(data: &'a [F], window: Window<F, WT>) -> Self {
-        WindowedFrame {
-            data: data,
-            window: window,
-            idx: 0
+    /// Construct a new `Windowed` signal.
+    pub fn new(signal: S, window: Window<S::Item, W>) -> Self {
+        Windowed {
+            signal_with_window: signal.zip(window),
         }
     }
 }
 
-impl<'a, F, WT> Iterator for WindowedFrame<'a, F, WT> 
+impl<'a, F, W> Windower<'a, F, W> 
     where F: 'a + Frame, 
-          <F as Frame>::Sample: FloatSample<Float=F::Sample> + FromSample<f64>, 
-          WT: Type<F::Sample>
+          W: Type
 {
-    type Item = F;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.window.next().and_then(|v| {
-            let out = self.data.get(self.idx)
-                .and_then(|d| Some(v.mul_amp(*d)));
-            self.idx += 1;
-            out
-        })
-    }
-}
-
-impl<'a, F, WT> Windower<'a, F, WT> 
-    where F: 'a + Frame, 
-          F::Sample: FromSample<f64>, 
-          WT: Type<F::Sample>
-{
-    pub fn new(data: &'a [F], bin: usize, hop: usize) -> Self {
+    /// Constructor for a new `Windower` iterator.
+    pub fn new(frames: &'a [F], bin: usize, hop: usize) -> Self {
         Windower {
             bin: bin,
             hop: hop,
             idx: 0,
-            data: data,
+            frames: frames,
             wttype: PhantomData
         }
     }
 }
 
-impl<'a, F, WT> Iterator for Windower<'a, F, WT> 
-    where F: 'a + Frame, 
-          F::Sample: FromSample<f64>, 
-          WT: Type<F::Sample>
+impl<'a, F> Windower<'a, F, Rectangle>
+    where F: 'a + Frame,
 {
-    type Item = WindowedFrame<'a, F, WT>;
+    /// Constructor for a `Windower` using the `Rectangle` window function.
+    pub fn rectangle(frames: &'a [F], bin: usize, hop: usize) -> Self {
+        Windower::new(frames, bin, hop)
+    }
+}
+
+impl<'a, F> Windower<'a, F, Hanning>
+    where F: 'a + Frame,
+{
+    /// Constructor for a `Windower` using the `Hanning` window function.
+    pub fn hanning(frames: &'a [F], bin: usize, hop: usize) -> Self {
+        Windower::new(frames, bin, hop)
+    }
+}
+
+
+impl<F, W> Iterator for Window<F, W> 
+    where F: Frame, 
+          W: Type
+{
+    type Item = F;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let v = W::at_phase(self.phase.next_phase());
+        let v_f: <F::Sample as Sample>::Float = v.to_sample();
+        Some(F::from_fn(|_| v_f.to_sample::<F::Sample>()))
+    }
+}
+
+impl<S, W> Iterator for Windowed<S, W> 
+    where S: Signal, 
+          S::Item: Frame,
+          W: Type
+{
+    type Item = S::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.signal_with_window.next()
+            .map(|(f, w)| w.mul_amp(f.to_float_frame()))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.signal_with_window.size_hint()
+    }
+}
+
+impl<'a, F, W> Iterator for Windower<'a, F, W> 
+    where F: 'a + Frame, 
+          W: Type
+{
+    type Item = Windowed<core::iter::Cloned<core::slice::Iter<'a, F>>, W>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let top = self.idx + self.bin;
-        if top < self.data.len() {
-            let data = &self.data[self.idx..top];
-            let window: Window<F, WT> = Window::new(self.bin);
+        if top < self.frames.len() {
+            let data = &self.frames[self.idx..top];
+            let window: Window<F, W> = Window::new(self.bin);
             self.idx += self.hop;
-            Some(WindowedFrame::new(data, window))
+            Some(Windowed::new(data.iter().cloned(), window))
         } else {
             None
         }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let rem = (self.data.len() - self.idx) - (self.bin - self.hop);
-        (super::ceil(rem as f64 / self.hop as f64) as usize, None)
+        let rem = (self.frames.len() - self.idx) - (self.bin - self.hop);
+        (super::ops::f64::ceil(rem as f64 / self.hop as f64) as usize, None)
     }
 }
 
-pub fn hanning<F>(len: usize) -> Window<F, Hanning> 
+
+/// A helper function for constructing a `Window` that uses a `Hanning` `Type` function.
+pub fn hanning<F>(num_frames: usize) -> Window<F, Hanning> 
     where F: Frame,
-          F::Sample: FromSample<f64> + ToSample<f64> 
 {
-    Window::new(len)
+    Window::new(num_frames)
 }
 
-pub fn rectangle<F>(len: usize) -> Window<F, Rectangle> 
+/// A helper function for constructing a `Window` that uses a `Rectangle` `Type` function.
+pub fn rectangle<F>(num_frames: usize) -> Window<F, Rectangle> 
     where F: Frame,
-          F::Sample: FromSample<f64> 
 {
-    Window::new(len)
+    Window::new(num_frames)
 }
-
--- tests/window.rs
+++ tests/window.rs
@@ -1,13 +1,13 @@
 extern crate sample;
 
 use sample::frame::Frame;
-use sample::window::{Windower, Rectangle};
+use sample::window::Windower;
 
 #[test]
 fn test_window_at_phase() {
     let window = sample::window::hanning::<[f64; 1]>(9);
-    let exp = [0., 0.1464, 0.5000, 0.8536, 1., 0.8536, 0.5000, 0.1464, 0., 0.1464];
-    for (r, e) in window.zip(exp.iter()) {
+    let expected = [0.0, 0.1464, 0.5000, 0.8536, 1., 0.8536, 0.5000, 0.1464, 0., 0.1464];
+    for (r, e) in window.zip(expected.iter()) {
         println!("Exp: {}\t\tFound: {}", e, r[0]);
         assert!((r[0] - e).abs() < 0.001);
     }
@@ -15,12 +15,13 @@ fn test_window_at_phase() {
 
 #[test]
 fn test_windower() {
-    let data: [[f64; 1]; 8] = [[0.1], [0.1], [0.2], [0.2], [0.3], [0.3], [0.4], [0.4]];
-    let exp = [[[0.1], [0.1]], [[0.1], [0.2]], [[0.2], [0.2]], [[0.2], [0.3]], [[0.3], [0.3]], [[0.3], [0.4]], [[0.4], [0.4]]];
+    let data = [[0.1f64], [0.1], [0.2], [0.2], [0.3], [0.3], [0.4], [0.4]];
+    let expected = [[[0.1], [0.1]], [[0.1], [0.2]], [[0.2], [0.2]], [[0.2], [0.3]],
+                    [[0.3], [0.3]], [[0.3], [0.4]], [[0.4], [0.4]]];
 
-    let windower: Windower<[f64; 1], Rectangle> = Windower::new(&data, 2, 1);
-    for (re, ex) in windower.zip(exp.iter()) {
-        for (r, e) in re.zip(ex.iter()) {
+    let windower = Windower::rectangle(&data, 2, 1);
+    for (chunk, expected_chunk) in windower.zip(expected.iter()) {
+        for (r, e) in chunk.zip(expected_chunk.iter()) {
             for (r_chan, e_chan) in r.channels().zip(e.channels()) {
                 println!("Exp: {}\t\tFound: {}", e_chan, r_chan);
                 assert!((r_chan - e_chan).abs() < 0.001);
