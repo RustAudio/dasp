127f78fd3333cfb4d9e5e7b05dd2f47603f58765
Andrew Smith <andrewchristophersmith@gmail.com>
2016-08-14 22:15:29 -0700

    Make Converter own the source

--- src/interpolate.rs
+++ src/interpolate.rs
@@ -1,7 +1,5 @@
 use {Duplex, Frame, Sample};
 
-use core::marker::PhantomData;
-
 /// An iterator that converts the rate at which frames are yielded from some given frame
 /// Interpolator into a new type.
 ///
@@ -13,62 +11,51 @@ use core::marker::PhantomData;
 /// - Sample decimator
 ///
 #[derive(Clone)]
-pub struct Converter<T: Iterator, I: Interpolator<T>>
+pub struct Converter<T: Iterator, I: Interpolator>
     where <T as Iterator>::Item: Frame
 {
+    source: T,
     interpolator: I,
     interpolation_value: f64,
-    source_to_target_ratio: f64,
-    phantom: PhantomData<T>
+    source_to_target_ratio: f64
 }
 
 /// Interpolator that just rounds off any values to the previous value from the source
-pub struct Floor<I>
-    where I: Iterator,
-          I::Item: Frame
+pub struct Floor<F>
 {
-    source: I,
-    left: I::Item 
+    left: F
 }
 
 /// Interpolator that interpolates linearly between the previous value and the next value
-pub struct Linear<I>
-    where I: Iterator,
-          I::Item: Frame
+pub struct Linear<F>
 {
-    source: I,
-    left: I::Item,
-    right: Option<I::Item>
+    left: F,
+    right: Option<F>
 }
 
 /// Trait for all things that can interpolate between two values. Implementations should keep track
 /// of the necessary data both before and after the current frame.
-pub trait Interpolator<I>
-    where I: Iterator,
-          I::Item: Frame
+pub trait Interpolator
 {
-    /// Creates a new Interpolator using the provided Iterator as the source
-    fn from_source(source: I) -> Self;
-
-    fn source(&self) -> &I;
+    type Frame: Frame;
 
     /// Given a distance between [0. and 1.) to the following sample, return the interpolated value
-    fn value_at(&self, x: f64) -> I::Item;
+    fn interpolate(&self, x: f64) -> Self::Frame;
 
     /// Called whenever the Interpolator value is over 1.
-    fn increment_frame(&mut self);
+    fn next_source_frame(&mut self, source_frame: Option<Self::Frame>);
 }
 
 impl<T, I> Converter<T, I> 
     where T: Iterator,
           <T as Iterator>::Item: Frame,
-          I: Interpolator<T>
+          I: Interpolator
 {
     /// Construct a new `Converter` from the source frames and the source and target sample rates
     /// (in Hz).
     #[inline]
-    pub fn from_hz_to_hz(interpolator: I, source_hz: f64, target_hz: f64) -> Self {
-        Self::scale_playback_hz(interpolator, source_hz / target_hz)
+    pub fn from_hz_to_hz(source: T, interpolator: I, source_hz: f64, target_hz: f64) -> Self {
+        Self::scale_playback_hz(source, interpolator, source_hz / target_hz)
     }
 
     /// Construct a new `Converter` from the source frames and the amount by which the current
@@ -78,13 +65,13 @@ impl<T, I> Converter<T, I>
     /// For example, if our `source_frames` is a sine wave oscillating at a frequency of 2hz and
     /// we wanted to convert it to a frequency of 3hz, the given `scale` should be `1.5`.
     #[inline]
-    pub fn scale_playback_hz(interpolator: I, scale: f64) -> Self {
+    pub fn scale_playback_hz(source: T, interpolator: I, scale: f64) -> Self {
         assert!(scale > 0.0, "We can't yield any frames at 0 times a second!");
         Converter {
+            source: source,
             interpolator: interpolator,
             interpolation_value: 0.0,
-            source_to_target_ratio: scale,
-            phantom: PhantomData
+            source_to_target_ratio: scale
         }
     }
 
@@ -97,8 +84,8 @@ impl<T, I> Converter<T, I>
     ///
     /// This is the same as calling `Converter::scale_playback_hz(source_frames, 1.0 / scale)`.
     #[inline]
-    pub fn scale_sample_hz(interpolator: I, scale: f64) -> Self {
-        Self::scale_playback_hz(interpolator, 1.0 / scale)
+    pub fn scale_sample_hz(source: T, interpolator: I, scale: f64) -> Self {
+        Self::scale_playback_hz(source, interpolator, 1.0 / scale)
     }
 
     /// Update the `source_to_target_ratio` internally given the source and target hz.
@@ -127,20 +114,20 @@ impl<T, I> Converter<T, I>
 
     /// Borrow the `source_frames` Interpolator from the `Converter`.
     #[inline]
-    pub fn source(&self) -> &I {
-        &self.interpolator
+    pub fn source(&self) -> &T {
+        &self.source
     }
 
     /// Mutably borrow the `source_frames` Iterator from the `Converter`.
     #[inline]
-    pub fn source_mut(&mut self) -> &I {
-        &mut self.interpolator
+    pub fn source_mut(&mut self) -> &mut T {
+        &mut self.source
     }
 
     /// Drop `self` and return the internal `source_frames` Iterator.
     #[inline]
-    pub fn into_source(self) -> I {
-        self.interpolator
+    pub fn into_source(self) -> T {
+        self.source
     }
 }
 
@@ -148,25 +135,25 @@ impl<T, I> Iterator for Converter<T, I>
     where T: Iterator,
           <T as Iterator>::Item: Frame,
           <<T as Iterator>::Item as Frame>::Sample: Duplex<f64>,
-          I: Interpolator<T>
+          I: Interpolator<Frame=<T as Iterator>::Item>
 {
     type Item = <T as Iterator>::Item;
 
     #[allow(unused_variables)]
-    fn next(&mut self) -> Option<<T as Iterator>::Item> {
+    fn next(&mut self) -> Option<Self::Item> {
         let Converter {
+            ref mut source, 
             ref mut interpolator,
             ref mut interpolation_value,
-            source_to_target_ratio,
-            phantom
+            source_to_target_ratio
         } = *self;
  
         while *interpolation_value >= 1.0 {
-            interpolator.increment_frame();
+            interpolator.next_source_frame(source.next());
             *interpolation_value -= 1.0;
         }
 
-        let out = Some(interpolator.value_at(*interpolation_value));
+        let out = Some(interpolator.interpolate(*interpolation_value));
         *interpolation_value += source_to_target_ratio;
 
         out
@@ -175,75 +162,49 @@ impl<T, I> Iterator for Converter<T, I>
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
         let len_multiplier = self.source_to_target_ratio / 1.0;
-        let (source_lower, source_upper) = self.interpolator.source().size_hint();
+        let (source_lower, source_upper) = self.source.size_hint();
         let lower = (source_lower as f64 * len_multiplier) as usize;
         let upper = source_upper.map(|upper| (upper as f64 * len_multiplier) as usize);
         (lower, upper)
     }
 }
 
-impl<I> Interpolator<I> for Floor<I>
-    where I: Iterator,
-          <I as Iterator>::Item: Frame,
-          <<I as Iterator>::Item as Frame>::Sample: Duplex<f64>
+impl<F> Interpolator for Floor<F>
+    where F: Frame,
+          <F as Frame>::Sample: Duplex<f64>
 {
-    fn from_source(mut source: I) -> Floor<I> {
-        let left = source.next().unwrap_or(I::Item::equilibrium());
+    type Frame = F;
 
-        Floor {
-            source: source,
-            left: left
-        }
-    }
-
-    fn source(&self) -> &I {
-        &self.source
-    }
-
-    fn value_at(&self, _x: f64) -> I::Item {
+    fn interpolate(&self, _x: f64) -> Self::Frame {
         self.left
     }
 
-    fn increment_frame(&mut self) {
-        self.left = self.source.next().unwrap_or(self.left);
+    fn next_source_frame(&mut self, source_frame: Option<Self::Frame>) {
+        self.left = source_frame.unwrap_or(self.left);
     }
 }
 
-impl<I> Interpolator<I> for Linear<I>
-    where I: Iterator,
-          <I as Iterator>::Item: Frame,
-          <<I as Iterator>::Item as Frame>::Sample: Duplex<f64>
+impl<F> Interpolator for Linear<F>
+    where F: Frame,
+          <F as Frame>::Sample: Duplex<f64>
 {
-    fn from_source(mut source: I) -> Linear<I> {
-        let left = source.next().unwrap_or(<<I as Iterator>::Item as Frame>::equilibrium());
-        let right = source.next();
-
-        Linear {
-            source: source,
-            left: left,
-            right: right
-        }
-    }
-
-    fn source(&self) -> &I {
-        &self.source
-    }
+    type Frame = F;
 
     /// Converts linearly from the previous value, using the next value to interpolate. It is
     /// possible, although not advisable, to provide an x > 1.0 or < 0.0, but this will just
     /// continue to be a linear ramp in one direction or another.
-    fn value_at(&self, x: f64) -> I::Item {
-        <<I as Iterator>::Item as Frame>::from_fn(|idx| {
-            let left = self.left.channel(idx).unwrap_or(&<<I as Iterator>::Item as Frame>::Sample::equilibrium()).to_sample::<f64>();
-            let right = self.right.unwrap_or(self.left).channel(idx).unwrap_or(&<<I as Iterator>::Item as Frame>::Sample::equilibrium()).to_sample::<f64>();
+    fn interpolate(&self, x: f64) -> Self::Frame {
+        <Self::Frame as Frame>::from_fn(|idx| {
+            let left = self.left.channel(idx).unwrap_or(&<Self::Frame as Frame>::Sample::equilibrium()).to_sample::<f64>();
+            let right = self.right.unwrap_or(self.left).channel(idx).unwrap_or(&<Self::Frame as Frame>::Sample::equilibrium()).to_sample::<f64>();
             let diff = right - left;
-            ((diff * x) + left).to_sample::<<<I as Iterator>::Item as Frame>::Sample>()
+            ((diff * x) + left).to_sample::<<Self::Frame as Frame>::Sample>()
         })
     }
 
-    fn increment_frame(&mut self) {
+    fn next_source_frame(&mut self, source_frame: Option<Self::Frame>) {
         self.left = self.right.unwrap_or(self.left);
-        self.right = self.source.next();
+        self.right = source_frame;
     }
 }
 
