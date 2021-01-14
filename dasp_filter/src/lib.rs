1116064f27e310ccd2f3bbb0d257e210aac9ac7b
Mark LeMoine <linclelinkpart5@gmail.com>
2021-01-13 20:03:58 -0800

    Rename `Coefficient` field names. Add doctest for `Biquad::apply`

--- dasp_filter/src/lib.rs
+++ dasp_filter/src/lib.rs
@@ -13,14 +13,14 @@ pub struct Coefficients<S>
 where
     S: FloatSample,
 {
-    // Numerator coefficients (normally b0, b1, b2).
-    pub n0: S,
-    pub n1: S,
-    pub n2: S,
+    // Transfer function numerator coefficients.
+    pub b0: S,
+    pub b1: S,
+    pub b2: S,
 
-    // Denominator coefficients (normally a1, a2).
-    pub d1: S,
-    pub d2: S,
+    // Transfer function denominator coefficients.
+    pub a1: S,
+    pub a2: S,
 }
 
 /// An implementation of a digital biquad filter, using the Direct Form 2
@@ -53,6 +53,29 @@ where
 
     /// Performs a single iteration of this filter, calculating a new filtered
     /// `Frame` from an input `Frame`.
+    ///
+    /// ```rust
+    /// use dasp_filter::{Coefficients, Biquad};
+    ///
+    /// fn main() {
+    ///     // Notch boost filter.
+    ///     let co = Coefficients {
+    ///         b0: 1.0469127398708575f64,
+    ///         b1: -0.27732002669854483,
+    ///         b2: 0.8588151488168104,
+    ///         a1: -0.27732002669854483,
+    ///         a2: 0.9057278886876682,
+    ///     };
+    ///
+    ///     // Note that this type argument defines the format of the temporary
+    ///     // values, as well as the number of channels required for input
+    ///     // `Frame`s.
+    ///     let mut b = Biquad::<[f64; 2]>::new(co);
+    ///
+    ///     assert_eq!(b.apply([32i8, -64]), [33, -67]);
+    ///     assert_eq!(b.apply([0.1f32, -0.3]), [0.107943736, -0.32057875]);
+    /// }
+    /// ```
     pub fn apply<I>(&mut self, input: I) -> I
     where
         I: Frame<NumChannels = F::NumChannels>,
@@ -62,21 +85,21 @@ where
         let input: F = input.map(ToSample::to_sample_);
 
         // Calculate scaled inputs.
-        let input_by_n0 = input.scale_amp(self.coeff.n0);
-        let input_by_n1 = input.scale_amp(self.coeff.n1);
-        let input_by_n2 = input.scale_amp(self.coeff.n2);
+        let input_by_b0 = input.scale_amp(self.coeff.b0);
+        let input_by_b1 = input.scale_amp(self.coeff.b1);
+        let input_by_b2 = input.scale_amp(self.coeff.b2);
 
         // This is the new filtered `Frame`.
-        let output: F = self.t0.add_amp(input_by_n0);
+        let output: F = self.t0.add_amp(input_by_b0);
 
         // Calculate scaled outputs.
         // NOTE: Negative signs on the scaling factors for these.
-        let output_by_neg_d1 = output.scale_amp(-self.coeff.d1);
-        let output_by_neg_d2 = output.scale_amp(-self.coeff.d2);
+        let output_by_neg_a1 = output.scale_amp(-self.coeff.a1);
+        let output_by_neg_a2 = output.scale_amp(-self.coeff.a2);
 
         // Update buffers.
-        self.t0 = self.t1.add_amp(input_by_n1).add_amp(output_by_neg_d1);
-        self.t1 = input_by_n2.add_amp(output_by_neg_d2);
+        self.t0 = self.t1.add_amp(input_by_b1).add_amp(output_by_neg_a1);
+        self.t1 = input_by_b2.add_amp(output_by_neg_a2);
 
         // Convert back into the original `Frame` format.
         output.map(FromSample::from_sample_)
