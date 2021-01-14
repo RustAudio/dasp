864b97144c3ddd6acc3fbda17778cdc7879cfee5
Mark LeMoine <linclelinkpart5@gmail.com>
2021-01-13 16:59:22 -0800

    Add `Coefficients` struct to `dasp_filter`

--- dasp_filter/src/lib.rs
+++ dasp_filter/src/lib.rs
@@ -1,3 +1,25 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(core_intrinsics))]
+
+use dasp_sample::FloatSample;
+
+/// Coefficients for a digital biquad filter.
+/// It is assumed that the `a0` coefficient is always normalized to 1.0,
+/// and thus not included.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Coefficients<S>
+where
+    S: FloatSample,
+{
+    // Numerator coefficients.
+    pub b0: S,
+    pub b1: S,
+    pub b2: S,
+
+    // Denominator coefficients.
+    pub a1: S,
+    pub a2: S,
+}
+
+
