83b352d88f6683ed951041f7941d1f9f9e583c2b
Mark LeMoine <linclelinkpart5@gmail.com>
2021-01-13 19:22:08 -0800

    Cleanup `Coefficient`/`Biquad` field names

--- dasp_filter/src/lib.rs
+++ dasp_filter/src/lib.rs
@@ -13,14 +13,14 @@ pub struct Coefficients<S>
 where
     S: FloatSample,
 {
-    // Numerator coefficients.
-    pub b0: S,
-    pub b1: S,
-    pub b2: S,
+    // Numerator coefficients (normally b0, b1, b2).
+    pub n0: S,
+    pub n1: S,
+    pub n2: S,
 
-    // Denominator coefficients.
-    pub a1: S,
-    pub a2: S,
+    // Denominator coefficients (normally a1, a2).
+    pub d1: S,
+    pub d2: S,
 }
 
 /// An implementation of a digital biquad filter, using the Direct Form 2
@@ -30,12 +30,12 @@ where
     F: Frame,
     F::Sample: FloatSample,
 {
-    coeff: Coefficients<F::Sample>,
+    pub coeff: Coefficients<F::Sample>,
 
     // Since biquad filters are second-order, we require two historical buffers.
     // This state is updated each time the filter is applied to a `Frame`.
-    m0: F,
-    m1: F,
+    t0: F,
+    t1: F,
 }
 
 impl<F> Biquad<F>
@@ -46,8 +46,8 @@ where
     pub fn new(coeff: Coefficients<F::Sample>) -> Self {
         Self {
             coeff,
-            m0: F::EQUILIBRIUM,
-            m1: F::EQUILIBRIUM,
+            t0: F::EQUILIBRIUM,
+            t1: F::EQUILIBRIUM,
         }
     }
 
@@ -62,21 +62,21 @@ where
         let input: F = input.map(ToSample::to_sample_);
 
         // Calculate scaled inputs.
-        let input_by_b0 = input.scale_amp(self.coeff.b0);
-        let input_by_b1 = input.scale_amp(self.coeff.b1);
-        let input_by_b2 = input.scale_amp(self.coeff.b2);
+        let input_by_n0 = input.scale_amp(self.coeff.n0);
+        let input_by_n1 = input.scale_amp(self.coeff.n1);
+        let input_by_n2 = input.scale_amp(self.coeff.n2);
 
         // This is the new filtered `Frame`.
-        let output: F = self.m0.add_amp(input_by_b0);
+        let output: F = self.t0.add_amp(input_by_n0);
 
         // Calculate scaled outputs.
         // NOTE: Negative signs on the scaling factors for these.
-        let output_by_neg_a1 = output.scale_amp(-self.coeff.a1);
-        let output_by_neg_a2 = output.scale_amp(-self.coeff.a2);
+        let output_by_neg_d1 = output.scale_amp(-self.coeff.d1);
+        let output_by_neg_d2 = output.scale_amp(-self.coeff.d2);
 
         // Update buffers.
-        self.m0 = self.m1.add_amp(input_by_b1).add_amp(output_by_neg_a1);
-        self.m1 = input_by_b2.add_amp(output_by_neg_a2);
+        self.t0 = self.t1.add_amp(input_by_n1).add_amp(output_by_neg_d1);
+        self.t1 = input_by_n2.add_amp(output_by_neg_d2);
 
         // Convert back into the original `Frame` format.
         output.map(FromSample::from_sample_)
