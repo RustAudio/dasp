22558ea4aa18383eab1a542e6fcba6ec50f7b12c
Mark LeMoine <linclelinkpart5@gmail.com>
2021-01-14 16:41:20 -0800

    Add doctest for `SignalFilter`. Add impl of `SignalFilter` for all `Signal`s

--- dasp_signal/src/filter.rs
+++ dasp_signal/src/filter.rs
@@ -12,17 +12,36 @@ use dasp_sample::{Sample, FromSample};
 
 /// An extension to the **Signal** trait that enables iterative filtering.
 ///
+/// # Example
+///
+/// ```
+/// use dasp_filter::{self as filter, Coefficients};
+/// use dasp_signal::{self as signal, Signal};
+/// use dasp_signal::filter::SignalFilter;
+///
+/// fn main() {
+///     let signal = signal::rate(48000.0).const_hz(997.0).sine();
+///     // Notch filter to attenuate 997 Hz.
+///     let coeff = Coefficients {
+///         b0: 0.9157328640471359f64,
+///         b1: -1.8158910212730535,
+///         b2: 0.9157328640471359,
+///         a1: -1.8158910212730535,
+///         a2: 0.831465728094272,
+///     };
+///     let mut filtered = signal.filtered(coeff);
+///     assert_eq!(
+///         filtered.take(4).collect::<Vec<_>>(),
+///         vec![0.0, 0.11917058366454024, 0.21640079287630784, 0.2938740006664008]
+///     );
+/// }
+/// ```
+///
 /// ### Required Features
 ///
 /// - When using `dasp_signal`, this item requires the **filter** feature to be enabled.
 /// - When using `dasp`, this item requires the **signal-filter** feature to be enabled.
 pub trait SignalFilter: Signal {
-    /// An adaptor that calculates and yields a filtered signal.
-    ///
-    /// ### Required Features
-    ///
-    /// - When using `dasp_signal`, this item requires the **filter** feature to be enabled.
-    /// - When using `dasp`, this item requires the **signal-filter** feature to be enabled.
     fn filtered(
         self,
         coeff: filter::Coefficients<<<Self::Frame as Frame>::Sample as Sample>::Float>,
@@ -40,6 +59,12 @@ pub trait SignalFilter: Signal {
     }
 }
 
+/// An adaptor that calculates and yields a filtered signal.
+///
+/// ### Required Features
+///
+/// - When using `dasp_signal`, this item requires the **filter** feature to be enabled.
+/// - When using `dasp`, this item requires the **signal-filter** feature to be enabled.
 pub struct FilteredSignal<S>
 where
     S: Signal,
@@ -65,3 +90,6 @@ where
         self.signal.is_exhausted()
     }
 }
+
+// Impl this for all `Signal`s.
+impl<T> SignalFilter for T where T: Signal {}
